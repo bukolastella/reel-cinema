a8437785f958bf5f1e9cf402001780c6ab2da3c6
Buks <abolarinbukolastella@gmail.com>
2022-03-05 21:10:06 +0100

    added fixes v2

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,9 +33,9 @@ const index: React.FC<ColorfulCircle> = (props) => {
   console.log(props.data[0]);
 
   return (
-    <div>
+    <>
       <Hero data={props.data} genreData={props.genreData} />
-    </div>
+    </>
   );
 };
 
@@ -46,13 +46,13 @@ export async function getStaticProps(): Promise<
     `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&page=1`
   );
   const result = await res.json();
-  const data = result?.results;
+  const data = JSON.parse(JSON.stringify(result.results));
 
   const genreRes = await fetch(
     `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US`
   );
   const genreResult = await genreRes.json();
-  const genreData = genreResult?.genres;
+  const genreData = JSON.parse(JSON.stringify(genreResult?.genres));
 
   return {
     props: {
